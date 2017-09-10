36f84998903802a12a626ef93384bd75ac8e18b4
rtfmpliz <rtfmpliz@gmail.com>
2017-09-10 22:09:10 +0700

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
+
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
 import { HttpModule } from "@angular/http";
 
-import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard.component';
-import { HeroDetailComponent } from './hero-detail.component';
-import { HeroesComponent } from './heroes.component';
-import { HeroService } from './hero.service';
-
 import { AppRoutingModule } from './app-routing.module';
-
 import { InMemoryWebApiModule } from "angular-in-memory-web-api/in-memory-web-api.module";
 import { InMemoryDataService } from "./in-memory-data.service";
+
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
+import { HeroSearchComponent } from './hero-search.component';
+import { HeroSearchService } from './hero-search.service';
 
 @NgModule({
     imports: [
@@ -26,7 +29,8 @@ import { InMemoryDataService } from "./in-memory-data.service";
     AppComponent,
     DashboardComponent,
     HeroDetailComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroSearchComponent
   ],
 
   providers: [HeroService],
